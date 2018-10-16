3320e69af3210ac78bf7e8576ad9bba08c49320b
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-16 14:34:45 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,8 @@ def first_challenge
 
   #your code here
   contacts.each do |name, data|
-    data.each do 
+    data.each do |key, value|
+      
   #remember to return your newly altered contacts hash!
   contacts
 end
